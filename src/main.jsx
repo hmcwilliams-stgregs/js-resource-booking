c1c08f2a37eb3c7b2c7766f06db3ac82dcc36b1d
hmcwilliams-stgregs <hmcwilliams@stgregs.nsw.edu.au>
2026-08-24 15:45:32 +1000

    Integrate MSAL for Azure authentication in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <div style={{ maxWidth: 1200, margin: "24px auto", padding: "0 16px" }}>
-      <App />
-    </div>
-  </React.StrictMode>
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+
+import App from "./App";
+import { msalConfig } from "./authConfig";
+
+const msalInstance =
+  new PublicClientApplication(msalConfig);
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <MsalProvider instance={msalInstance}>
+    <App />
+  </MsalProvider>
 );
